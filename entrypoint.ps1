007b3c794b9767355313e66f9270c819ec0a25e5
Yasin-Amadmia <ckyconsulting.uk@gmail.com>
2020-11-18 10:24:16 +0000

    Update entrypoint.ps1

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -1,5 +1,5 @@
 for ($i=1; $i -le 10; $i++) {
-  Write-Host "Attempting to run winlogbeat"
+  Write-Host "Attempting to run filebeat"
   $process = Start-Process -FilePath filebeat.exe -Wait -PassThru -ArgumentList $args
   if ($process.ExitCode -eq 0) { break }
   Start-Sleep 10
