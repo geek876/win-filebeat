95f0b47fbbb814ba87b157bcf54a2a9f3d46ddcb
Yasin-Amadmia <ckyconsulting.uk@gmail.com>
2020-11-18 11:49:11 +0000

    Update entrypoint.ps1

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -1,6 +1,6 @@
 for ($i=1; $i -le 10; $i++) {
   Write-Host "Attempting to run filebeat"
-  $process = Start-Process -FilePath filebeat.exe -Wait -PassThru -ArgumentList $args
+  $process = Start-Process -FilePath filebeat.exe -Wait -PassThru -ArgumentList  $env:args
   if ($process.ExitCode -eq 0) { break }
   Start-Sleep 10
 }
